61a031b5516f05b70545bec5de1dd2f2e6b04f55
JesusVillalon <jesus.villalon@bosonit.com>
2023-09-04 14:46:50 +0200

    circle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainCircleComponent } from './main-circle/main-circle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCircleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-circle/main-circle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-circle',
+  templateUrl: './main-circle.component.html',
+  styleUrls: ['./main-circle.component.css']
+})
+export class MainCircleComponent {
+
+}

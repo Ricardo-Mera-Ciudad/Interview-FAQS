1f70473ff82b725dfb9da56553202af854317493
Jesus Villalon <villalongallardo92@gmail.com>
2023-09-05 13:25:29 +0200

    circle updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainCircleComponent } from './main-circle/main-circle.component';
-
 
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-
-    MainCircleComponent
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule, 
-    SharedModule,
-    PagesModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, PagesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

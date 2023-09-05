952d302f03287c95fd3a2e4710472a86c89ca969
Ricardo-Mera-Ciudad <ricardo.mera@bosonit.com>
2023-09-05 09:05:15 +0200

    new footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LevelcardsComponent } from './components/levelcards/levelcards.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [LevelcardsComponent,
-  HeaderComponent],
+  HeaderComponent,
+  FooterComponent],
   imports: [
     CommonModule
   ], 
   exports: [
     LevelcardsComponent,
-    HeaderComponent
-
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }

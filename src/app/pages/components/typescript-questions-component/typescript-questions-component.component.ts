491d1c23afad1188620de3222a4536b32b926f2a
Jesus Villalon <villalongallardo92@gmail.com>
2023-09-06 12:09:54 +0200

    routes created

---
+++ src/app/pages/components/typescript-questions-component/typescript-questions-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typecript-questions-component',
+  templateUrl: './typescript-questions-component.component.html',
+  styleUrls: ['./typescript-questions-component.component.css']
+})
+export class TypescriptQuestionsComponentComponent {
+
+}

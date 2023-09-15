5627fae2f4e9af381e7e7ed0d7f9d87c8bfd8f7a
Lola García <mariadolores.garcia@bosonit.com>
2023-09-15 14:08:59 +0200

    delete icon aspa

--- src/app/auth/services/users.service.ts
+++ src/app/auth/services/users.service.ts
@@ -4,88 +4,78 @@ import { environments } from 'src/environment/environment';
 import { Observable, Subject, tap, map, catchError, of, switchMap } from 'rxjs';
 import { UserData } from 'src/app/shared/interfaces/user-data.interface';
 
-
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-
   private baseUrl: string = environments.baseUrl;
   private user?: UserData;
   public userAdded = new Subject<UserData>();
   public userToUpdate = new Subject<UserData>();
-  public token = "";
-  
+  public token = '';
 
   constructor(private http: HttpClient) {}
 
-  addUser(user: UserData): Observable<UserData>{
-    return this.http.post<UserData>(`${this.baseUrl}/users`, user)
-    .pipe(
+  addUser(user: UserData): Observable<UserData> {
+    return this.http.post<UserData>(`${this.baseUrl}/users`, user).pipe(
       tap((addedUser) => {
-        this.userAdded.next(addedUser)
+        this.userAdded.next(addedUser);
       })
-    )
+    );
   }
 
   getAddedUser(): Observable<UserData> {
-    return this.userAdded.asObservable()
+    return this.userAdded.asObservable();
   }
 
   updateUser(user: UserData): Observable<UserData> {
-    if(!user.id) throw Error('User id is required');
-    return this.http.patch<UserData>(`${this.baseUrl}/users/${user.id}`, user)
-            .pipe(
-              tap(updatedUser => this.userToUpdate.next(updatedUser))
-            )
+    if (!user.id) throw Error('User id is required');
+    return this.http
+      .patch<UserData>(`${this.baseUrl}/users/${user.id}`, user)
+      .pipe(tap((updatedUser) => this.userToUpdate.next(updatedUser)));
   }
 
   getUpdatedUser(): Observable<UserData> {
     return this.userToUpdate.asObservable();
   }
 
-  deleteUserById(id: number): Observable<boolean>{
-    return this.http.delete(`${this.baseUrl}/users/${id}`)
-          .pipe(
-            map(() => true),
-            catchError(() => of(false))
-          )
+  deleteUserById(id: number): Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/users/${id}`).pipe(
+      map(() => true),
+      catchError(() => of(false))
+    );
   }
 
-  createJwtToken(payload:any) {
+  createJwtToken(payload: any) {
     const base64Url = btoa(JSON.stringify(payload));
     return base64Url;
   }
 
+  login(email: string, password: string): Observable<UserData | null> {
+    return this.http.get<UserData[]>(`${this.baseUrl}/users`).pipe(
+      switchMap((users) => {
+        const user = users.find(
+          (u) => u.email === email && u.password === password
+        );
+        if (user) {
+          console.log(user);
+          const tokenPayload = {
+            sub: user.id,
+            email: user.email,
+          };
+          this.token = this.createJwtToken(tokenPayload);
+          localStorage.setItem('authToken', this.token);
+        }
+        return of(user || null); 
+      }),
+      catchError(() => {
+        return of(null);
+      })
+    );
+  }
 
-login(email: string, password: string): Observable<UserData | null> {
-  return this.http.get<UserData[]>(`${this.baseUrl}/users`).pipe(
-    switchMap(users => {
-      const user = users.find(u => u.email === email && u.password === password);
-      if (user) {
-        const tokenPayload = {
-          sub: user.id,
-          email: user.email,
-        };
-        this.token = this.createJwtToken(tokenPayload);
-        localStorage.setItem('authToken', this.token);
-      }
-      return of(user || null); // Devuelve el usuario o null
-    }),
-    catchError(() => {
-      // Manejar errores de autenticación aquí
-      return of(null);
-    })
-  );
-}
-
-logout() {
-  this.user = undefined;
-  localStorage.removeItem('authToken');
-}
-
-
-
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem('authToken');
+  }
 }

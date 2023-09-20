a4b3f1da4d40ec6c07691c813a827bc7497755d7
Lola García <canal030w@gmail.com>
2023-09-20 17:27:40 +0200

    add data to edit form

--- src/app/pages/components/edit-user/edit-user.component.ts
+++ src/app/pages/components/edit-user/edit-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { UserData } from 'src/app/shared/interfaces/user-data.interface';
 import { UsersService } from 'src/app/auth/services/users.service';
 import { ActivatedRoute } from '@angular/router';
@@ -8,9 +8,9 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.css'],
 })
-export class EditUserComponent {
+export class EditUserComponent implements OnInit {
   
-  public user?: UserData | null;
+  public user: UserData | null = null;
 
   constructor(
     private usersService: UsersService,
@@ -23,27 +23,23 @@ export class EditUserComponent {
 
   getUserIdByUrl(){
     const userId = this.route.snapshot.params['id'];
+    console.log('ID del usuario:', userId);
 
-    this.usersService.getUserById(userId).subscribe(
-      (userResponse: UserData) => {
-        this.user = userResponse;
-      },
-      (error) => {
-        console.error('Error al obtener la información del usuario:', error);
-      }
-    );
+    this.usersService.getUserById(userId)
+      .subscribe((userData: UserData) => {
+      this.user = userData;
+    });
   }
 
   onSubmit() {
     if (this.user) {
-      this.usersService.updateUser(this.user).subscribe(
-        (updatedUser: UserData) => {
-          console.log('Usuario actualizado:', updatedUser);
-        },
-        (error) => {
-          console.error('Error al actualizar el usuario:', error);
-        }
-      );
+      this.usersService.updateUser(this.user).subscribe((user) => {
+        console.log(user);
+        this.user = user;
+      });
+    } else {
+      console.error('No se puede enviar el formulario porque "user" es nulo o indefinido.');
     }
   }
 }
+

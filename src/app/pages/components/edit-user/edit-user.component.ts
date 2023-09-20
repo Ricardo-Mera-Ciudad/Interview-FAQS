837bcc68314c59bd4ae2b13cab053ad787193679
Lola García <canal030w@gmail.com>
2023-09-20 16:51:25 +0200

    add style to edit form

--- src/app/pages/components/edit-user/edit-user.component.ts
+++ src/app/pages/components/edit-user/edit-user.component.ts
@@ -3,29 +3,47 @@ import { UserData } from 'src/app/shared/interfaces/user-data.interface';
 import { UsersService } from 'src/app/auth/services/users.service';
 import { ActivatedRoute } from '@angular/router';
 
-
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
-  styleUrls: ['./edit-user.component.css']
+  styleUrls: ['./edit-user.component.css'],
 })
 export class EditUserComponent {
-  @Input() user?: UserData | null;
+  
+  public user?: UserData | null;
 
   constructor(
     private usersService: UsersService,
-    private route:ActivatedRoute
-    ) { 
-      const userId = this.route.snapshot.params['id'];
-      console.log('ID del usuario:', userId); 
+    private route: ActivatedRoute
+  ) {}
 
-    }
+  ngOnInit(): void {
+    this.getUserIdByUrl();
+  }
+
+  getUserIdByUrl(){
+    const userId = this.route.snapshot.params['id'];
+
+    this.usersService.getUserById(userId).subscribe(
+      (userResponse: UserData) => {
+        this.user = userResponse;
+      },
+      (error) => {
+        console.error('Error al obtener la información del usuario:', error);
+      }
+    );
+  }
 
   onSubmit() {
-    this.usersService.updateUser(this.user)
-    .subscribe((user) => {
-      console.log(user);
-      this.user = user;
-    });
+    if (this.user) {
+      this.usersService.updateUser(this.user).subscribe(
+        (updatedUser: UserData) => {
+          console.log('Usuario actualizado:', updatedUser);
+        },
+        (error) => {
+          console.error('Error al actualizar el usuario:', error);
+        }
+      );
+    }
   }
 }
